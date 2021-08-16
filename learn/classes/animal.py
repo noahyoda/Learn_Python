d419c08ba9002c492c6052a9c903c3351620f06f
noahyoda <noahyoda1@gmail.com>
2021-08-16 11:36:27 -1000

    added function to animal object

--- learn/classes/animal.py
+++ learn/classes/animal.py
@@ -3,3 +3,9 @@ class animal:
         self.diet = diet
         self.name = name
         self.waterlogged = waterlogged
+
+    def breath_air(self):
+        if self.waterlogged == True:
+            return False
+        else:
+            return True
---
+++ learn/classes/zoo.py
@@ -0,0 +1,6 @@
+from animal import animal
+
+monkey = animal("omnivore", "gibbon", False)
+
+print(monkey.name)
+print(monkey.breath_air())
